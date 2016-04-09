3dfd3de63f2fe8e53eac85d77d5e988f121ee52c
David Sudia <dsudia@gmail.com>
2016-04-08 20:54:21 -0600

    initial commit

---
+++ com/teamtreehouse/docgen/Doc.java
@@ -0,0 +1,15 @@
+package com.teamtreehouse.docgen;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Doc {
+
+    /** Description of class or method */
+
+
+    /** Description of parameters, if annotated element is a method & has parameters */
+
+    /** Description of return value, if annotated element is a method & isn't void */
+}
